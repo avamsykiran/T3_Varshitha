77e0c8e97941279756ad8fa640709690467fd388
dachepalli srinivas <dachepallisrinivas@gmail.com>
2021-06-01 11:16:55 +0530

    Day-1

---
+++ JavaSEWorkspace/ToStartWith/src/com/tp/tsw/fundamental/BiggerOfTwo.java
@@ -0,0 +1,25 @@
+package com.tp.tsw.fundamental;
+
+import java.util.Scanner;
+
+public class BiggerOfTwo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int firstNumber, secondNumber;
+		System.out.println("Enter first number : ");
+		firstNumber = scan.nextInt();
+		System.out.println("Enter second number : ");
+		secondNumber = scan.nextInt();
+		
+		
+		if (firstNumber > secondNumber)
+			System.out.println(firstNumber + " is Big");
+		else
+			System.out.println(secondNumber + " is Big");
+		
+		scan.close();
+	}
+
+}
